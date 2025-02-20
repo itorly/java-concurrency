d0a027dd927ef43132860522e6ae549d0a8f0e6f
itorly <itorly@163.com>
2025-02-20 22:59:18 +0800

    Synchronized this; Synchronized instance method; Synchronized static method

--- src/main/java/org/itorly/jc/synchronize/SynchronizedObj.java
+++ src/main/java/org/itorly/jc/synchronize/SynchronizedObj.java
@@ -3,12 +3,32 @@ package org.itorly.jc.synchronize;
 public class SynchronizedObj {
 
     private int count = 0;
-    private Object o = new Object();
+    private final Object o = new Object();
 
-    public void add() {
+    public void add1() {
         synchronized (o) {
             System.out.println(Thread.currentThread().getName() + ":" + ++count);
         }
+    }
+
+    public void add2() {
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + ":" + ++count);
+        }
+    }
+
+    public synchronized void add3() {
+        System.out.println(Thread.currentThread().getName() + ":" + ++count);
+    }
+
+    public static void add4() {
+        synchronized (SynchronizedObj.class) {
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+    public synchronized static void add5() {
+        System.out.println(Thread.currentThread().getName());
     }
 
     public int getCount() {
--- src/test/java/org/itorly/jc/SynchronizeTest.java
+++ src/test/java/org/itorly/jc/SynchronizeTest.java
@@ -3,14 +3,16 @@ package org.itorly.jc;
 import org.itorly.jc.synchronize.SynchronizedObj;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SynchronizeTest {
 
     @Test
-    public void testSynchronizedObj() throws InterruptedException {
-        SynchronizedObj synchronizedObj = new SynchronizedObj();
+    public void testSynchronizedObj() throws InterruptedException, NoSuchMethodException {
+        SynchronizedObj synchronizedObj;
 
         // Number of threads to create
         int numThreads = 10;
@@ -19,25 +21,60 @@ public class SynchronizeTest {
         // Create an array to hold the threads
         Thread[] threads = new Thread[numThreads];
 
-        // Create and start the threads
-        for (int i = 0; i < numThreads; i++) {
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < incrementsPerThread; j++) {
-                    synchronizedObj.add(); // Call the synchronized method
-                }
-            });
-            threads[i].start();
+        String[] methodNames = new String[]{
+                "add1",
+                "add2",
+                "add3",
+                "add4",
+                "add5"
+        };
+
+        for (int h = 0; h < methodNames.length; h++) {
+            // Reset the count for each method test
+            synchronizedObj = new SynchronizedObj();
+            // Get the method to call using reflection
+            Method method;
+            if (methodNames[h].equals("add4") || methodNames[h].equals("add5")) {
+                // Static methods
+                method = SynchronizedObj.class.getMethod(methodNames[h]);
+            } else {
+                // Instance methods
+                method = SynchronizedObj.class.getMethod(methodNames[h]);
+            }
+
+            // Create and start the threads
+            for (int i = 0; i < numThreads; i++) {
+                int finalH = h;
+                SynchronizedObj finalSynchronizedObj = synchronizedObj;
+                threads[i] = new Thread(() -> {
+                    for (int j = 0; j < incrementsPerThread; j++) {
+                        try {
+                            if (methodNames[finalH].equals("add4") || methodNames[finalH].equals("add5")) {
+                                // Call static methods
+                                method.invoke(null);
+                            } else {
+                                // Call instance methods
+                                method.invoke(finalSynchronizedObj);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, "Thread-" + methodNames[h] + "-" + i);
+                threads[i].start();
+            }
+
+            // Wait for all threads to finish
+            for (Thread thread : threads) {
+                thread.join();
+            }
+
+            // Verify the final value of count
+            if (!methodNames[h].equals("add4") && !methodNames[h].equals("add5")) {
+                int expectedCount = (numThreads * incrementsPerThread); // Initial count is 0
+                assertEquals(expectedCount, synchronizedObj.getCount(), "The final count value is incorrect.");
+            }
         }
-
-        // Wait for all threads to finish
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        // Verify the final value of count
-        int expectedCount = (numThreads * incrementsPerThread); // Initial count is 0
-        assertEquals(expectedCount, synchronizedObj.getCount(), "The final count value is incorrect.");
-
 
     }
 }
