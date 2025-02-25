50efe26bb580bffc6f52e1fba614395e89314e12
itorly <itorly@163.com>
2025-02-25 23:59:20 +0800

    Exception Causing Release

---
+++ src/main/java/org/itorly/jc/synchronize/ExceptionCausingRelease.java
@@ -0,0 +1,21 @@
+package org.itorly.jc.synchronize;
+
+public class ExceptionCausingRelease {
+
+    private int count = 0;
+
+    synchronized void add() {
+        System.out.println("Thread " + Thread.currentThread().getName() + " start");
+        while (true) {
+            count++;
+            System.out.println("Thread " + Thread.currentThread().getName() + " now count = " + count);
+            if (count == 3) {
+                throw new NullPointerException("Human-induced Exception");
+            }
+            if (count == 10) {
+                throw new NullPointerException("Test is over.");
+            }
+        }
+    }
+
+}
