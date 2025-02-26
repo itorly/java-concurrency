66e0638379598f7ffb0356953dd6ab3bd3000211
itorly <itorly@163.com>
2025-02-26 22:08:30 +0800

    add "count" variable in SyncAndAnotherMethod

--- src/main/java/org/itorly/jc/synchronize/SyncAndAnotherMethod.java
+++ src/main/java/org/itorly/jc/synchronize/SyncAndAnotherMethod.java
@@ -2,8 +2,12 @@ package org.itorly.jc.synchronize;
 
 public class SyncAndAnotherMethod {
 
+    private int count = 0;
+
     public synchronized void one() {
         System.out.println(Thread.currentThread().getName() + " start one method");
+        ++count;
+        System.out.println("In one method, count = " + count);
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -13,7 +17,10 @@ public class SyncAndAnotherMethod {
     }
 
     public void two() {
-        System.out.println(Thread.currentThread().getName() + " execute two method");
+        System.out.println(Thread.currentThread().getName() + " start two method");
+        ++count;
+        System.out.println("In two method, count = " + count);
+        System.out.println(Thread.currentThread().getName() + " end two method");
     }
 
 }
